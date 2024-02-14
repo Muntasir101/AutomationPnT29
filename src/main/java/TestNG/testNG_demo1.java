7ba585eaebccab266a3e775eab17c7a82f851a03
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-02-14 15:12:06 +0600

    TestNG TN Product search

--- src/main/java/TestNG/testNG_demo1.java
+++ src/main/java/TestNG/testNG_demo1.java
@@ -1,14 +1,28 @@
 package TestNG;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class testNG_demo1 {
+
+    @BeforeMethod
+    public void setup(){
+        System.out.println("Before Method Executed.");
+    }
 
     @Test
     public void testCase1()  {
         System.out.println("Test case 1 Executed.");
     }
+
     @Test
     public void testCase2()  {
         System.out.println("Test case 2 Executed.");
     }
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("After Method Executed.");
+    }
+
 }
